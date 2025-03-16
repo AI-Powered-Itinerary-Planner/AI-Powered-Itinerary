683cee974491b260711acb16a17a1633c7ac3c95
Z-ABBASI <101zabbasi@gmail.com>
2025-03-15 18:49:32 -0700

    Explore Page Interface

--- frontend/src/pages/Explore.jsx
+++ frontend/src/pages/Explore.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import "./Explore.css";
+import { useNavigate } from 'react-router-dom';
 
 const Explore = () => {
+  const navigate = useNavigate();
+
   return (
     <div>
-      Explore
+      <h4>Outdoor activities</h4>
+      <textarea></textarea>
+      <h4>Shops and landscapes</h4>
+      <textarea></textarea>
+      <button onClick={() => navigate('/plantrip')}>What's the plan?</button>
     </div>
   )
 }
