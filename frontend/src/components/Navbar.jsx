544529c85a2546f5d33208f47fc3117d19846eac
Amancio Ramirez <amancs.r.23@gmail.com>
2025-03-19 17:28:16 -0700

    Fixed navbar to automatically close when navigating

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -6,6 +6,8 @@ import "./Navbar.css"
 
 const Navbar = () => {
     const [menuOpen, setMenuOpen] = useState(false)
+
+    const closeMenu = () => setMenuOpen(false)
   return <nav>
     <Link to="/" className="title">AI-POWERED ITINERARY</Link>
     <div className="menu" onClick={() =>{
@@ -16,13 +18,13 @@ const Navbar = () => {
         <span></span>
     </div>
     <ul className={menuOpen ? "open" : ""}>
-        <li><NavLink to="/home">Home</NavLink></li>
-        <li><NavLink to="/explore">Explore</NavLink></li>
-        <li><NavLink to="/plantrip">Plan Trip</NavLink></li>
-        <li><NavLink to="/saved-itineary">Saved Itineary</NavLink></li>
-        <li><NavLink to="/register">Register</NavLink></li>
-        <li><NavLink to="/login">Login</NavLink></li>
-        <li><NavLink to="/settings">Settings</NavLink></li>
+        <li><NavLink to="/home" onClick={closeMenu}>Home</NavLink></li>
+        <li><NavLink to="/explore" onClick={closeMenu}>Explore</NavLink></li>
+        <li><NavLink to="/plantrip" onClick={closeMenu}>Plan Trip</NavLink></li>
+        <li><NavLink to="/saved-itineary" onClick={closeMenu}>Saved Itineary</NavLink></li>
+        <li><NavLink to="/register" onClick={closeMenu}>Register</NavLink></li>
+        <li><NavLink to="/login" onClick={closeMenu}>Login</NavLink></li>
+        <li><NavLink to="/settings" onClick={closeMenu}>Settings</NavLink></li>
         {/* <li><NavLink to="/about">About</NavLink></li> */}
     </ul>
   </nav>
